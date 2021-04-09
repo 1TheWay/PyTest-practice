576a2bc61f1751ea714543ffe97dab0d99745a11
Ivan <>
2021-04-09 21:37:48 +0400

    correct name

--- test_items.py
+++ test_items.py
@@ -2,8 +2,8 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_garbage(browser):
+def test_guest_should_see_basket(browser):
     browser.get(link)
     #time.sleep(5)
     item = browser.find_element_by_class_name("btn-add-to-basket")
-    assert item!=None, "There isn't the garbage"
+    assert item!=None, "There isn't the basket"
