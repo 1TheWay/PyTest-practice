4fa2b24c9c3e0913d181c65333eb78d0b221e780
Ivan <>
2021-04-09 21:36:09 +0400

    correct link and assert

--- test_items.py
+++ test_items.py
@@ -1,8 +1,9 @@
-#import time
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_garbage(browser):
     browser.get(link)
     #time.sleep(5)
-    browser.find_element_by_class_name("btn-add-to-basket")
+    item = browser.find_element_by_class_name("btn-add-to-basket")
+    assert item!=None, "There isn't the garbage"
