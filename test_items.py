bb73902debc9f4ce12269d6cfbfa185a2c71774b
Ivan <>
2021-04-09 20:56:52 +0400

    correct link

--- test_items.py
+++ test_items.py
@@ -1,5 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+#import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_garbage(browser):
     browser.get(link)
+    #time.sleep(5)
     browser.find_element_by_class_name("btn-add-to-basket")
