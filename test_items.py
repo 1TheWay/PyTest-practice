793dc9b173b37491cacf514da36f0c2e690cba47
Ivan <>
2021-04-10 11:29:37 +0400

    element->elementS for correct assert

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_basket(browser):
     browser.get(link)
     #time.sleep(5)
-    item = browser.find_element_by_class_name("btn-add-to-basket")
-    assert item!=None, "There isn't the basket"
+    item = browser.find_elements_by_class_name("btn-add-to-basket")
+    assert len(item)!=0, "There isn't the basket"
